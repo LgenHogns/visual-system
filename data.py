2c0f59cb8b2a3eb467292f94227d7475e2f596bc
LgenHogns <lgenhogns@163.com>
2021-08-22 16:27:31 +0800

    一些改动

---
+++ data.py
@@ -0,0 +1,310 @@
+# coding=utf-8
+
+import pymysql
+import configparser
+from dbutils.persistent_db import PersistentDB
+
+
+class DBData:
+    def __init__(self):
+        cnf = configparser.ConfigParser()
+        cnf.read('settings.ini')
+
+        self.pool = PersistentDB(
+            creator=pymysql,  # 使用链接数据库的模块
+            maxusage=None,  # 一个链接最多被重复使用的次数，None表示无限制
+            setsession=[],  # 开始会话前执行的命令列表
+            ping=0,  # ping MySQL服务端，检查是否服务可用。# 如：0 = None = never, 1 = default = whenever it is requested, 2 = when a cursor is created, 4 = when a query is executed, 7 = always
+            closeable=False,  # 如果为False时， conn.close() 实际上被忽略，供下次使用，再线程关闭时，才会自动关闭链接。如果为True时， conn.close()则关闭链接，那么再次调用pool.connection时就会报错，因为已经真的关闭了连接
+            threadlocal=None,  # 本线程独享值得对象，用于保存链接对象，如果链接对象被重置
+            host=cnf.get('mysql', 'db_ip'),
+            port=int(cnf.get('mysql', 'db_port')),
+            user=cnf.get('mysql', 'db_user'),
+            passwd=cnf.get('mysql', 'db_password'),
+            db=cnf.get('mysql', 'db_database')
+        )
+        
+        db = self.pool.connection(shareable=False)
+        cursor = db.cursor()
+
+        cursor.execute('''
+            create table if not exists now_tid(
+                num int
+            )
+        ''')
+
+        cursor.execute('''
+            create table if not exists cardinality(
+                tid int,
+                num int
+            )
+        ''')
+
+        cursor.execute('''
+            create table if not exists entropy(
+                tid int,
+                num double
+            )
+        ''')
+
+        cursor.execute('''
+            create table if not exists heavy_hitter(
+                tid int,
+                src_ip int unsigned,
+                dst_ip int unsigned,
+                src_port smallint unsigned,
+                dst_port int unsigned,
+                protocol int,
+                size int unsigned
+            )
+        ''')
+
+        cursor.execute('''
+            create table if not exists heavy_change(
+                tid int,
+                src_ip int unsigned,
+                dst_ip int unsigned,
+                src_port smallint unsigned,
+                dst_port int unsigned,
+                protocol int,
+                increment int
+            )
+        ''')
+
+        cursor.execute('''
+            create table if not exists flows(
+                tid int,
+                src_ip int unsigned,
+                dst_ip int unsigned,
+                src_port smallint unsigned,
+                dst_port int unsigned,
+                protocol int,
+                size int
+            )
+        ''')
+
+        cursor.execute('''
+            create table if not exists latency(
+                tid int,
+                src_ip int unsigned,
+                dst_ip int unsigned,
+                src_port smallint unsigned,
+                dst_port int unsigned,
+                protocol int,
+                switch_id int
+            )
+        ''')
+
+        cursor.execute('''
+            create table if not exists distribution(
+                tid int,
+                idx int,
+                num double
+            )
+        ''')
+
+        db.commit()
+        cursor.close()
+        db.close()
+
+    def set_tid(self, tid):
+        db = self.pool.connection(shareable=False)
+        cursor = db.cursor()
+
+        query = 'insert into now_tid values (%s)'
+        cursor.execute(query, tid)
+
+        db.commit()
+        cursor.close()
+        db.close()
+
+    def get_tid(self):
+        db = self.pool.connection(shareable=False)
+        cursor = db.cursor()
+
+        query = 'select max(num) from now_tid'
+        cursor.execute(query)
+        result = cursor.fetchall()
+
+        db.commit()
+        cursor.close()
+        db.close()
+
+        return result
+
+    def set_heavy_hitter(self, tid, value):
+        db = self.pool.connection(shareable=False)
+        cursor = db.cursor()
+
+        query = 'insert into heavy_hitter values (' + \
+            str(tid)+',%s,%s,%s,%s,%s,%s)'
+        cursor.executemany(query, value)
+
+        db.commit()
+        cursor.close()
+        db.close()
+
+    def get_heavy_hitter(self):
+        db = self.pool.connection(shareable=False)
+        cursor = db.cursor()
+
+        query = 'select * from heavy_hitter order by size desc limit 2000'
+        cursor.execute(query)
+        result = cursor.fetchall()
+
+        db.commit()
+        cursor.close()
+        db.close()
+
+        return result
+
+    def set_heavy_change(self, tid, value):
+        db = self.pool.connection(shareable=False)
+        cursor = db.cursor()
+
+        query = 'insert into heavy_change values (' + \
+            str(tid)+',%s,%s,%s,%s,%s,%s)'
+        cursor.executemany(query, value)
+
+        db.commit()
+        cursor.close()
+        db.close()
+
+    def get_heavy_change(self):
+        db = self.pool.connection(shareable=False)
+        cursor = db.cursor()
+
+        query = 'select * from heavy_change order by increment desc limit 2000'
+        cursor.execute(query)
+        result = cursor.fetchall()
+
+        db.commit()
+        cursor.close()
+        db.close()
+
+        return result
+
+    def set_switch(self, tid, value):
+        db = self.pool.connection(shareable=False)
+        cursor = db.cursor()
+
+        query = 'insert into latency values ('+str(tid)+',%s,%s,%s,%s,%s,%s)'
+        cursor.executemany(query, value)
+
+        db.commit()
+        cursor.close()
+        db.close()
+
+    def get_switch(self):
+        db = self.pool.connection(shareable=False)
+        cursor = db.cursor()
+
+        query = 'select * from latency limit 200'
+        cursor.execute(query)
+        result = cursor.fetchall()
+
+        db.commit()
+        cursor.close()
+        db.close()
+
+        return result
+
+    def set_distri(self, tid, value):
+        db = self.pool.connection(shareable=False)
+        cursor = db.cursor()
+
+        query = 'insert into distribution values ('+str(tid)+',%s,%s)'
+        cursor.executemany(query, value)
+
+        db.commit()
+        cursor.close()
+        db.close()
+
+    def get_distri(self):
+        db = self.pool.connection(shareable=False)
+        cursor = db.cursor()
+
+        query = 'select * from distribution order by idx'
+        cursor.execute(query)
+        result = cursor.fetchall()
+
+        db.commit()
+        cursor.close()
+        db.close()
+
+        return result
+
+    def set_size(self, tid, value):
+        db = self.pool.connection(shareable=False)
+        cursor = db.cursor()
+
+        query = 'insert into flows values ('+str(tid)+',%s,%s,%s,%s,%s,%s)'
+        cursor.executemany(query, value)
+
+        db.commit()
+        cursor.close()
+        db.close()
+
+    def get_size(self):
+        db = self.pool.connection(shareable=False)
+        cursor = db.cursor()
+
+        query = 'select * from flows limit 200'
+        cursor.execute(query)
+        result = cursor.fetchall()
+
+        db.commit()
+        cursor.close()
+        db.close()
+
+        return result
+
+    def set_cardinality(self, tid, value):
+        db = self.pool.connection(shareable=False)
+        cursor = db.cursor()
+
+        query = 'insert into cardinality values ('+str(tid)+',%s)'
+        cursor.execute(query, (value))
+
+        db.commit()
+        cursor.close()
+        db.close()
+
+    def get_cardinality(self):
+        db = self.pool.connection(shareable=False)
+        cursor = db.cursor()
+
+        query = 'select * from cardinality'
+        cursor.execute(query)
+        result = cursor.fetchall()
+
+        db.commit()
+        cursor.close()
+        db.close()
+
+        return result
+
+    def set_entropy(self, tid, value):
+        db = self.pool.connection(shareable=False)
+        cursor = db.cursor()
+
+        query = 'insert into entropy values ('+str(tid)+',%s)'
+        cursor.execute(query, (value))
+
+        db.commit()
+        cursor.close()
+        db.close()
+
+    def get_entropy(self):
+        db = self.pool.connection(shareable=False)
+        cursor = db.cursor()
+
+        query = 'select * from entropy'
+        cursor.execute(query)
+        result = cursor.fetchall()
+
+        db.commit()
+        cursor.close()
+        db.close()
+
+        return result
--- webserver.py
+++ webserver.py
@@ -1,6 +1,43 @@
 from flask import Flask, render_template, request, redirect, url_for, Blueprint
 import json
 import numpy as np
+from data import DBData
+import threading
+import random
+
+
+class RWlock(object):
+    def __init__(self):
+        self._lock = threading.Lock()
+        self._extra = threading.Lock()
+        self.read_num = 0
+
+    def read_acquire(self):
+        with self._extra:
+            self.read_num += 1
+            if self.read_num == 1:
+                self._lock.acquire()
+
+    def read_release(self):
+        with self._extra:
+            self.read_num -= 1
+            if self.read_num == 0:
+                self._lock.release()
+
+    def write_acquire(self):
+        self._lock.acquire()
+
+    def write_release(self):
+        self._lock.release()
+
+
+def int2ip(x):
+    return '.'.join(
+        [str(x//(256**i) % 256) for i in range(3, -1, -1)])
+
+
+def takeSixth(item):
+    return item[5]
 
 
 class CustomEncoder(json.JSONEncoder):
@@ -15,8 +52,16 @@ class CustomEncoder(json.JSONEncoder):
             return super(CustomEncoder, self).default(obj)
 
 
-def get_data(data_name):
-    return data[data_name]
+def get_color(x):
+    colors = [[0x2e, 0xc7, 0xc9],
+              [0xb6, 0xa2, 0xde],
+              [0x5a, 0xb1, 0xef],
+              [0xff, 0xb9, 0x80],
+              [0xd8, 0x7a, 0x80],
+              [0x8d, 0x98, 0xb3],
+              [0xe5, 0xcf, 0x0d],
+              [0x97, 0xb5, 0x52]]
+    return '#%02x%02x%02x' % (colors[x % 8][0], colors[x % 8][1], colors[x % 8][2])
 
 
 def get_flow_size():
@@ -24,14 +69,14 @@ def get_flow_size():
     获取Flow size estimation页面所需数据
     '''
     result = []
-    items = get_data('flow_size')
+    items = json_data['flows']
+    random.shuffle(items)
     for idx, item in enumerate(items):
         t = []
         t.append('<tr>')
-        t.append('<td>' + str(idx+1) + '</td>')
-        t.append('<td>' + str(item[0]) + '</td>')
-        t.append('<td>' + str(item[1]) + '</td>')
+        t.append('<td>' + int2ip(item[0]) + '</td>')
         t.append('<td>' + str(item[2]) + '</td>')
+        t.append('<td>' + int2ip(item[1]) + '</td>')
         t.append('<td>' + str(item[3]) + '</td>')
         t.append('<td>' + str(item[4]) + '</td>')
         t.append('<td>' + str(item[5]) + '</td>')
@@ -44,36 +89,101 @@ def get_heavy_hitter():
     '''
     获取Heavy hitter detection页面所需数据
     '''
-    result = []
-    items = get_data('heavy_hitter')
+    flow_data = []
+    total_data = {}
+    items = json_data['heavy_hitter']
+    items.sort(key=takeSixth, reverse=True)
     for idx, item in enumerate(items):
-        t = []
-        t.append('<tr>')
-        t.append('<td>' + str(idx+1) + '</td>')
-        t.append('<td>' + str(item[0]) + '</td>')
-        t.append('<td>' + str(item[1]) + '</td>')
-        t.append('<td>' + str(item[2]) + '</td>')
-        t.append('<td>' + str(item[3]) + '</td>')
-        t.append('<td>' + str(item[4]) + '</td>')
-        t.append('<td>' + str(item[5]) + '</td>')
-        t.append('</tr>')
-        result.append("".join(t))
-    return result
+        now = {
+            'name': '',
+            'value': [len(flow_data) + 1, item[5]],
+            'ID': idx,
+            'itemStyle': {
+                'color': get_color(len(flow_data) + 1),
+                'borderColor': '#d0d0d0',
+                'borderWidth': 2
+            }
+        }
+        flow_data.append(now)
+        total_data[item[5]] = total_data.get(item[5], 0) + 1
+
+    total_data1 = []
+    for key, value in total_data.items():
+        total_data1.append([key, value])
+
+    return {'flow_data': flow_data, 'total_data': list(total_data.items())}
 
 
 def get_heavy_change():
     '''
     获取Heavy change detection页面所需数据
     '''
+    flow_data = []
+    total_data = {}
+    items = json_data['heavy_change']
+    items.sort(key=takeSixth, reverse=True)
+    print(items)
+    for idx, item in enumerate(items):
+        now = {
+            'name': '',
+            'value': [len(flow_data) + 1, item[5]],
+            'ID': idx,
+            'itemStyle': {
+                'color': get_color(len(flow_data) + 1),
+                'borderColor': '#d0d0d0',
+                'borderWidth': 2
+            }
+        }
+        flow_data.append(now)
+        total_data[item[5]] = total_data.get(item[5], 0) + 1
+
+    total_data1 = []
+    for key, value in total_data.items():
+        total_data1.append([key, value])
+
+    return {'flow_data': flow_data, 'total_data': list(total_data.items())}
+
+
+def get_distri_entr():
+    '''
+    获取流分布和熵
+    '''
+    tmp = json_data['distribution']
+    card = json_data['cardinality']
+    num = [0, 0, 0, 0, 0, 0]
+    entropy = json_data['entropy']
+    for item in tmp:
+        print(item)
+        if item[1] > 0:
+            if item[0] <= 20:
+                num[0] += 1
+            elif 20 < item[0] <= 50:
+                num[1] += 1
+            elif 50 < item[0] <= 200:
+                num[2] += 1
+            elif 200 < item[0] <= 500:
+                num[3] += 1
+            elif 500 < item[0] <= 2048:
+                num[4] += 1
+            else:
+                num[5] += 1
+    return {
+        'flow_dis': num,
+        'entropy': round(entropy, 2),
+        'card': card
+    }
+
+
+def get_latency():
     result = []
-    items = get_data('heavy_change')
+    items = json_data['inflated_latency']
+    random.shuffle(items)
     for idx, item in enumerate(items):
         t = []
         t.append('<tr>')
-        t.append('<td>' + str(idx+1) + '</td>')
-        t.append('<td>' + str(item[0]) + '</td>')
-        t.append('<td>' + str(item[1]) + '</td>')
+        t.append('<td>' + int2ip(item[0]) + '</td>')
         t.append('<td>' + str(item[2]) + '</td>')
+        t.append('<td>' + int2ip(item[1]) + '</td>')
         t.append('<td>' + str(item[3]) + '</td>')
         t.append('<td>' + str(item[4]) + '</td>')
         t.append('<td>' + str(item[5]) + '</td>')
@@ -82,10 +192,14 @@ def get_heavy_change():
     return result
 
 
-data = {'flow_size': [[1, 1, 1, 1, 1, 1]],
-        'heavy_hitter': [[2, 2, 2, 2, 2, 2]],
-        'heavy_change': [[3, 3, 3, 3, 3, 3]]}
 app = Flask(__name__)
+json_data = {'flows': [],
+             'heavy_hitter': [],
+             'heavy_change': [],
+             'dirtribution': [],
+             'cardinality': 0,
+             'entropy': 0,
+             'inflated_latency': []}
 
 
 @app.route('/')
@@ -116,6 +230,20 @@ def show2():
     return render_template('show2.html', pageid=2, sidedata=chart)
 
 
+@app.route('/show/3')
+def show3():
+    dataid = 1
+    chart = {'dataid': dataid}
+    return render_template('show3.html', pageid=3, sidedata=chart)
+
+
+@app.route('/show/4')
+def show4():
+    dataid = 1
+    chart = {'dataid': dataid}
+    return render_template('show4.html', pageid=4, sidedata=chart)
+
+
 @app.route('/getdata', methods=['GET', 'POST'])
 def getdata():
     '''
@@ -124,13 +252,20 @@ def getdata():
     pageid = int(request.args.get('pageid'))
     dataid = request.args.get('dataid')
     result = []
+    print('getdata')
+    lock.read_acquire()
     if pageid == 0:
         result = get_flow_size()
     elif pageid == 1:
         result = get_heavy_hitter()
     elif pageid == 2:
         result = get_heavy_change()
-    print(result)
+    elif pageid == 3:
+        result = get_distri_entr()
+    else:
+        result = get_latency()
+    lock.read_release()
+    print('getdataend')
     return json.dumps(result, cls=CustomEncoder)
 
 
@@ -139,18 +274,46 @@ def postdata():
     '''
     以json格式接收数据
     '''
-    global data
+    global json_data
 
     rawdata = request.get_data(as_text=True)
 
+    lock.write_acquire()
     try:
-        data = json.loads(rawdata)
+        json_data = json.loads(rawdata)
     except:
         traceback.print_exc()
         return 'json syntax error'
+    lock.write_release()
+
+    lock1.acquire()
+
+    tid = data.get_tid()[0][0]+1
+
+    data.set_tid(tid)
+
+    data.set_size(tid, json_data['flows'])
+
+    data.set_heavy_hitter(tid, json_data['heavy_hitter'])
+
+    data.set_heavy_change(tid, json_data['heavy_change'])
+
+    data.set_distri(tid, json_data['distribution'])
+
+    data.set_switch(tid, json_data['inflated_latency'])
+
+    data.set_cardinality(tid, json_data['cardinality'])
+
+    data.set_entropy(tid, json_data['entropy'])
+    lock1.release()
 
     return 'data got'
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    lock = RWlock()
+    lock1 = threading.Lock()
+    lock1.acquire()
+    data = DBData()
+    lock1.release()
+    app.run(host='0.0.0.0', port=5000, debug=False)
